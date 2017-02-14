6273a4e13d5c8e9c84d9d2be02d6800b3a189494
Zach Edwards <tolklorist@gmail.com>
2017-02-14 08:14:16 -0600

    number 15

--- app/conway/alive-neighbors.js
+++ app/conway/alive-neighbors.js
@@ -1,1 +1,12 @@
-export default function () {}
+export default function (arr) {
+
+  let aliveNeighbors = 0;
+
+for (let i = 0; i <= arr.length; i += 1) {
+    if (arr[i]) {
+      aliveNeighbors += 1;
+    }
+  }
+
+  return aliveNeighbors;
+}
