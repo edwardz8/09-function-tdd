4deb138e9cac98e28d862a0665405190a7652698
Zach Edwards <tolklorist@gmail.com>
2017-02-13 18:37:34 -0600

    testing

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,8 +1,13 @@
-export default function (a , b) {
+export default function (array) {
 
-if {a{0} === b{0}} {
-  return true;
-}
+// declare a constant for the length of the array
+const length = array.length;
+// declare a variable for the sum of the array
+let sumArray = 0;
 
+  for (let i = 0; i < length; i++) {
+    sumArray += array[i];  }
 
+    // return the value of sumArray
+    return sumArray;
 }
--- app/conway/should-stay-alive.js
+++ app/conway/should-stay-alive.js
@@ -1,9 +1,3 @@
 import aliveNeighbors from './alive-neighbors';
 
-export default function (a) {
-  if (aliveNeighbors(a) >= 4 || aliveNeighbors(a) === 0) {
-    return false;
-  }
-
-  return true;
-}
+export default function () {
